0ac6fd9808133981d9c2f961ccf3191ff89601a7
akempins <01178535@pw.edu.pl>
2023-07-27 12:07:35 +0200

    Propably fixed request body

--- pdf_reader/main.py
+++ pdf_reader/main.py
@@ -7,6 +7,7 @@ from fastapi import (
     status,
     Request
     )
+from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from pdf_reader.reader import pdf_to_text_tesseract
 from pdf_reader.exceptions import KafkaUploadException
@@ -21,6 +22,7 @@ topic_name = os.environ['PDF_TEXT_TOPIC']
 
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -29,7 +31,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 kafka = KafkaProducer(bootstrap_servers=bootstrap_servers, api_version=(0, 10))
+
+
+class Body(BaseModel):
+    pdf_file: UploadFile
+    id: str
 
 
 @app.exception_handler(KafkaUploadException)
@@ -41,9 +49,10 @@ async def kafka_exception_handler(request: Request, exc: KafkaUploadException):
 
 
 @app.post('/api/reader')
-async def upload_pdf_file(pdf_file: UploadFile, id: str):
-    contents = await pdf_file.read()
-    text = pdf_to_text_tesseract(contents)
+async def upload_pdf_file(request: Request, body: Body):
+    pdf_contents = await body.pdf_file.read()
+    id = body.id
+    text = pdf_to_text_tesseract(pdf_contents)
     try:
         future = kafka.send(topic_name, key=id.encode("utf-8"), value=text.encode('utf-8'))
         metadata = future.get(timeout=10)
